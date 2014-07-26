0fe1557619eb7004956524bf44a056242347685e
Kamil Bazydlo <etesis@op.pl>
2014-07-26 12:11:40 +0200

    Updated bootstrap for usage in index

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Europe/Warsaw');
 function DHLAutoloader($class)
 {
     $class = str_replace('\\', '/', $class);
-    require '../lib/' . $class  . '.php';
+    require 'lib/' . $class  . '.php';
 }
 
 spl_autoload_register('DHLAutoloader');
